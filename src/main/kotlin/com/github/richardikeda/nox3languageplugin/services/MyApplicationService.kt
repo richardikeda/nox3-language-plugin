954b0c5d36609a28e6984045b020c021827ff143
GitHub Action <action@github.com>
2021-06-11 14:27:22 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/richardikeda/nox3languageplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.richardikeda.nox3languageplugin.services
+
+import com.github.richardikeda.nox3languageplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
