4b55eb272de28a7f961145f1d7173bf80c89df5a
Richard Ikeda <11233899+richardikeda@users.noreply.github.com>
2022-05-15 21:24:10 -0300

    Added generated files

---
+++ src/main/gen/com/enterscript/noX3LanguagePlugin/language/psi/NOX3Visitor.java
@@ -0,0 +1,18 @@
+// This is a generated file. Not intended for manual editing.
+package com.enterscript.noX3LanguagePlugin.language.psi;
+
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiElement;
+
+public class NOX3Visitor extends PsiElementVisitor {
+
+  public void visitProperty(@NotNull NOX3Property o) {
+    visitPsiElement(o);
+  }
+
+  public void visitPsiElement(@NotNull PsiElement o) {
+    visitElement(o);
+  }
+
+}
