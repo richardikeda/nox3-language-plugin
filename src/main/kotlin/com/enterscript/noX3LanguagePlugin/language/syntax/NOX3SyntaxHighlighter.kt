c50b3e0b1738da0dacc32b2215d7bf5c82e59379
Richard Ikeda <11233899+richardikeda@users.noreply.github.com>
2025-08-21 15:58:15 -0300

    Add completion, syntax highlighting, and structure view

--- src/main/kotlin/com/enterscript/noX3LanguagePlugin/language/syntax/NOX3SyntaxHighlighter.kt
+++ src/main/kotlin/com/enterscript/noX3LanguagePlugin/language/syntax/NOX3SyntaxHighlighter.kt
@@ -10,98 +10,30 @@ import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributes
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
-import org.jetbrains.annotations.NotNull
 
-
+/**
+ * Basic syntax highlighter for the X3 language. It recognises keywords,
+ * literals and both line and block comments.
+ */
 class NOX3SyntaxHighlighter : SyntaxHighlighterBase() {
-    internal companion object {
-        internal val SEPARATOR  = createTextAttributesKey("NOX3_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN)
-//
-        internal val KEY= createTextAttributesKey("NOX3_KEY", DefaultLanguageHighlighterColors.KEYWORD)
-        internal val VALUE= createTextAttributesKey("VALUE", DefaultLanguageHighlighterColors.STRING)
-        private val COMMENT= createTextAttributesKey("COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
-        private val COMMENT_MULTI_LINE= createTextAttributesKey("COMMENT_MULTI_LINE", DefaultLanguageHighlighterColors.BLOCK_COMMENT)
-        internal val BAD_CHARACTER = createTextAttributesKey("NOX3_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
-//        val BAD_CHAR_KEYS = arrayOf(BAD_CHARACTER)
+    companion object {
+        val KEYWORD: TextAttributesKey = createTextAttributesKey("NOX3_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD)
+        val STRING: TextAttributesKey = createTextAttributesKey("NOX3_STRING", DefaultLanguageHighlighterColors.STRING)
+        val COMMENT: TextAttributesKey = createTextAttributesKey("NOX3_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
+        val BAD_CHARACTER: TextAttributesKey = createTextAttributesKey("NOX3_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
+        private val SEPARATOR: TextAttributesKey = createTextAttributesKey("NOX3_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN)
 
-        private val SEPARATOR_KEYS = arrayOf(SEPARATOR)
-        private val KEY_KEYS = arrayOf(KEY)
-        private val VALUE_KEYS = arrayOf(VALUE)
-        private val COMMENT_KEYS = arrayOf(COMMENT)
-        private val COMMENT_MULTI_LINE_KEYS = arrayOf(COMMENT_MULTI_LINE)
-//        private val EMPTY_KEYS = arrayOfNulls<TextAttributesKey>(0)
-
-
-        private val keys = HashMap<IElementType, TextAttributesKey>()
-
-        init {
-            keys[TokenType.BAD_CHARACTER] = HighlighterColors.BAD_CHARACTER
-            keys[NOX3Types.COMMENT] = COMMENT
-//            keys[NOX3Types.COMMENT_MULTI_LINE] = COMMENT_MULTI_LINE
-            keys[NOX3Types.VALUE] = VALUE
-            keys[NOX3Types.KEY] = KEY
-            keys[NOX3Types.SEPARATOR] = SEPARATOR
-
-//            fillMap(COMMENT_KEYS, NOX3Types.COMMENT, COMMENT)
-
-//            keys.put(NOX3Types.COMMENT_MULTI_LINE, BLOCK_COMMENT)
-
-//            keys.put(NOX3Types.SECTION_NAME, SECTION_NAME)
-
-//            keys.put(NOX3Types.TYPE_NAME, TYPE_NAME)
-//            keys.put(NOX3Types.TYPE_NAME_COMPARABLE, TYPE_NAME_COMPARABLE)
-//            keys.put(NOX3Types.INSTRUCTION_TOKEN, INSTRUCTION)
-//            fillMap(keys, NOX3Types.MACROS, MACRO)
-
-//            keys.put(NOX3Types.TYPE_ANNOTATION_TOKEN, TYPE_ANNOTATION)
-//            keys.put(NOX3Types.VAR_ANNOTATION_TOKEN, VARIABLE_ANNOTATION)
-//            keys.put(NOX3Types.FIELD_ANNOTATION_TOKEN, FIELD_ANNOTATION)
-
-//            keys.put(NOX3Types.INT, INT_LITERAL)
-//            keys.put(NOX3Types.BYTE, BYTE_LITERAL)
-//            keys.put(NOX3Types.QUOTE, STRING_LITERAL)
-//            keys.put(NOX3Types.STRING_CONTENT, STRING_LITERAL)
-//            keys.put(NOX3Types.STRING_ESCAPE, VALID_STRING_ESCAPE)
-//            keys.put(NOX3Types.STRING_ESCAPE_INVALID, ILLEGAL_STRING_ESCAPE)
-//            keys.put(NOX3Types.TAG, TAG)
-
-//            keys.put(NOX3Types.SEMI, SEMI)
-//            fillMap(keys, NOX3TokenSets.PARENTHESES, PAREN)
-//            fillMap(keys, NOX3TokenSets.BRACES, BRACES)
+        private val keys = HashMap<IElementType, TextAttributesKey>().apply {
+            put(NOX3Types.KEY, KEYWORD)
+            put(NOX3Types.VALUE, STRING)
+            put(NOX3Types.COMMENT, COMMENT)
+            put(NOX3Types.COMMENT_MULTI_LINE, COMMENT)
+            put(NOX3Types.SEPARATOR, SEPARATOR)
+            put(TokenType.BAD_CHARACTER, BAD_CHARACTER)
         }
-
-
-
     }
 
-    @NotNull
     override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey> = pack(keys[tokenType])
 
-    @NotNull
     override fun getHighlightingLexer(): Lexer = NOX3LexerAdapter()
-
-//
-//    @NotNull
-//    override fun getHighlightingLexer(): Lexer = NOX3LexerAdapter()
-//
-//    override fun getTokenHighlights(tokenType: IElementType): Array<out TextAttributesKey?> {
-//        if (tokenType == NOX3Types.SEPARATOR) {
-//            return SEPARATOR_KEYS
-//        }
-//        if (tokenType == NOX3Types.KEY) {
-//            return KEY_KEYS
-//        }
-//        if (tokenType == NOX3Types.VALUE) {
-//            return VALUE_KEYS
-//        }
-//        if (tokenType == NOX3Types.COMMENT) {
-//            return COMMENT_KEYS
-//        }
-//        if (tokenType == TokenType.BAD_CHARACTER) {
-//            return BAD_CHAR_KEYS
-//        }
-//
-//        return EMPTY_KEYS
-//    }
-
 }
