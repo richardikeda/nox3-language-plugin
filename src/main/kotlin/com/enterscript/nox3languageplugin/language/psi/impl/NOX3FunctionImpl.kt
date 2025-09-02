a8b914de2cb345efb385ee86c30cc3a3b2695fa1
Richard Ikeda <11233899+richardikeda@users.noreply.github.com>
2025-09-02 16:06:05 -0300

    chore: remove generated grammar sources

--- src/main/kotlin/com/enterscript/nox3languageplugin/language/psi/impl/NOX3FunctionImpl.kt
+++
@@ -1,6 +0,0 @@
-package com.enterscript.nox3languageplugin.language.psi.impl
-
-import com.enterscript.nox3languageplugin.language.psi.NOX3Function
-import com.intellij.lang.ASTNode
-
-class NOX3FunctionImpl(node: ASTNode) : NOX3NamedElementImpl(node), NOX3Function
