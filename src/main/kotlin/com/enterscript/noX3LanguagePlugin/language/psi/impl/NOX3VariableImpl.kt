5e6c5408163d971d56d6faab3fb8d44d29c08249
Richard Ikeda <11233899+richardikeda@users.noreply.github.com>
2025-09-02 15:01:25 -0300

    refactor: generate grammars with grammarkit

--- src/main/kotlin/com/enterscript/noX3LanguagePlugin/language/psi/impl/NOX3VariableImpl.kt
+++
@@ -1,6 +0,0 @@
-package com.enterscript.noX3LanguagePlugin.language.psi.impl
-
-import com.enterscript.noX3LanguagePlugin.language.psi.NOX3Variable
-import com.intellij.lang.ASTNode
-
-class NOX3VariableImpl(node: ASTNode) : NOX3NamedElementImpl(node), NOX3Variable
