28d4b2e380a57a4396dfc1904a6623b5fa5ba93b
Richard Ikeda <11233899+richardikeda@users.noreply.github.com>
2025-09-03 23:54:24 -0300

    test: add lexer and parser tests

--- src/test/kotlin/com/enterscript/nox3languageplugin/language/lexer/NOX3LexerTest.kt
+++ src/test/kotlin/com/enterscript/nox3languageplugin/language/lexer/NOX3LexerTest.kt
@@ -8,17 +8,24 @@ import kotlin.test.assertEquals
 
 class NOX3LexerTest {
     @Test
-    fun testKeywordToken() {
+    fun testModuleToken() {
         val lexer = NOX3LexerAdapter(_NOX3Lexer(null))
-        lexer.start("case")
-        assertEquals(NOX3Types.CASE, lexer.tokenType)
+        lexer.start("module")
+        assertEquals(NOX3Types.MODULE, lexer.tokenType)
     }
 
     @Test
     fun testFunctionToken() {
         val lexer = NOX3LexerAdapter(_NOX3Lexer(null))
-        lexer.start("print")
-        assertEquals(NOX3Types.PRINT, lexer.tokenType)
+        lexer.start("function")
+        assertEquals(NOX3Types.FUNCTION, lexer.tokenType)
+    }
+
+    @Test
+    fun testVarToken() {
+        val lexer = NOX3LexerAdapter(_NOX3Lexer(null))
+        lexer.start("var")
+        assertEquals(NOX3Types.VAR, lexer.tokenType)
     }
 }
 
--- src/test/kotlin/com/enterscript/nox3languageplugin/language/parser/NOX3ParserTest.kt
+++ src/test/kotlin/com/enterscript/nox3languageplugin/language/parser/NOX3ParserTest.kt
@@ -2,7 +2,9 @@ package com.enterscript.nox3languageplugin.language.parser
 
 import com.enterscript.nox3languageplugin.language.NOX3FileType
 import com.enterscript.nox3languageplugin.language.psi.NOX3File
-import com.enterscript.nox3languageplugin.language.psi.NOX3Property
+import com.enterscript.nox3languageplugin.language.psi.NOX3Function
+import com.enterscript.nox3languageplugin.language.psi.NOX3Module
+import com.enterscript.nox3languageplugin.language.psi.NOX3Variable
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
@@ -10,12 +12,27 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class NOX3ParserTest : BasePlatformTestCase() {
-    fun testParseProperty() {
-        val file = PsiFileFactory.getInstance(project)
-            .createFileFromText("a.nox3", NOX3FileType.INSTANCE, "foo:bar") as NOX3File
-        val property = PsiTreeUtil.findChildOfType(file, NOX3Property::class.java)
-        assertNotNull(property)
-        assertEquals("foo", property.key)
-        assertEquals("bar", property.value)
+    private fun createFile(text: String): NOX3File {
+        return PsiFileFactory.getInstance(project)
+            .createFileFromText("test.nox3", NOX3FileType.INSTANCE, text) as NOX3File
+    }
+
+    fun testParseModuleFunctionVariable() {
+        val file = createFile(
+            """module Demo
+function foo
+end
+var bar
+end"""
+        )
+        val module = PsiTreeUtil.findChildOfType(file, NOX3Module::class.java)
+        assertNotNull(module)
+        val function = PsiTreeUtil.findChildOfType(module, NOX3Function::class.java)
+        assertNotNull(function)
+        assertEquals(module, PsiTreeUtil.getParentOfType(function, NOX3Module::class.java))
+        val variable = PsiTreeUtil.findChildOfType(module, NOX3Variable::class.java)
+        assertNotNull(variable)
+        assertEquals(module, PsiTreeUtil.getParentOfType(variable, NOX3Module::class.java))
     }
 }
+
