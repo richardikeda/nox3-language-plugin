301838f918c3238aa24103cd76b4fcea0ed8325b
Richard Ikeda <11233899+richardikeda@users.noreply.github.com>
2025-09-04 03:58:00 -0300

    Add completion test using file-based fixtures

--- src/test/kotlin/com/enterscript/nox3languageplugin/language/NOX3CompletionTest.kt
+++ src/test/kotlin/com/enterscript/nox3languageplugin/language/NOX3CompletionTest.kt
@@ -4,9 +4,13 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import kotlin.test.assertTrue
 
 class NOX3CompletionTest : BasePlatformTestCase() {
-    fun testCompletion() {
-        myFixture.configureByText("a.nox3", "<caret>")
+    override fun getTestDataPath(): String {
+        return "src/test/testData"
+    }
+
+    fun testCompletionFromFiles() {
+        myFixture.configureByFiles("completion/main.nox3", "completion/definitions.nox3")
         val result = myFixture.completeBasic().map { it.lookupString }
-        assertTrue(result.contains("Hello"))
+        assertTrue(result.contains("myVar"))
     }
 }
